eb2cc8c266a4fa8262e740a9b5d1dc7a1b693c2f
Kratika Gupta <therealkratikagupta@gmail.com>
2026-03-10 19:10:38 +0530

    render frontend link added

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://expense-tracker-1-e7lf.onrender.com',
     credentials: true,
   })
 );
