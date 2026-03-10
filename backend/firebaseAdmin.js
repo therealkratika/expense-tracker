854a00ca41a91dc7b8a4aa86e3481efcad199010
Kratika Gupta <therealkratikagupta@gmail.com>
2026-03-10 19:53:24 +0530

    fixed

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,15 +1,14 @@
 require("dotenv").config();
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
 
-if (!process.env.FIREBASE_PRIVATE_KEY) {
-  throw new Error("❌ FIREBASE_PRIVATE_KEY is missing");
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
 }
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
-});
 module.exports = admin;
