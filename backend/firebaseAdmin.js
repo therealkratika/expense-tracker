51c90a1b97afd2d419a7b1f0377fef01026a0c0d
Kratika Gupta <therealkratikagupta@gmail.com>
2026-03-10 19:00:00 +0530

    firebaseAdmin fixed

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,9 +1,15 @@
-const admin = require("firebase-admin");
+require("dotenv").config();
+var admin = require("firebase-admin");
 
-const serviceAccount = require("./src/expense-tracker-8d3fd-firebase-adminsdk-fbsvc-f7e740bc99.json");
+if (!process.env.FIREBASE_PRIVATE_KEY) {
+  throw new Error("❌ FIREBASE_PRIVATE_KEY is missing");
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
-
 module.exports = admin;
