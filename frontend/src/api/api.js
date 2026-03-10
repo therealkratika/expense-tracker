75634d2c084b72841f72c74e5806424e0767e3a3
Kratika Gupta <therealkratikagupta@gmail.com>
2026-03-10 20:09:38 +0530

    help

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,23 +1,20 @@
-import axios from "axios";
-import { auth } from "../firebase";
+import axios from 'axios';
+import { auth } from '../firebase';
 
 const api = axios.create({
-  baseURL: "https://expense-tracker-4v4b.onrender.com",
+  baseURL: 'https://expense-tracker-4v4b.onrender.com',
 });
 
 api.interceptors.request.use(async (config) => {
-
   const user = auth.currentUser;
 
   if (user) {
     const token = await user.getIdToken();
+    console.log('Sending token:', token);
     config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
-
-}, (error) => {
-  return Promise.reject(error);
 });
 
 export default api;
